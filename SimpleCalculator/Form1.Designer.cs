9eb53c5f7c9eff0eff727301c330f28f2c9fa4c4
DELL P75F <vanixgzy@gmail.com>
2021-10-09 12:00:34 +0700

    Add project files.

---
+++ SimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace SimpleCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOperand1 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSubstract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtOperand1
+            // 
+            this.txtOperand1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOperand1.Location = new System.Drawing.Point(231, 38);
+            this.txtOperand1.Margin = new System.Windows.Forms.Padding(4);
+            this.txtOperand1.Name = "txtOperand1";
+            this.txtOperand1.Size = new System.Drawing.Size(301, 39);
+            this.txtOperand1.TabIndex = 1;
+            this.txtOperand1.Text = "0.00";
+            this.txtOperand1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtOperand1_KeyPress);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(27, 236);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(132, 53);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(231, 144);
+            this.txtResult.Margin = new System.Windows.Forms.Padding(4);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(301, 39);
+            this.txtResult.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 148);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Result";
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOperand2.Location = new System.Drawing.Point(231, 90);
+            this.txtOperand2.Margin = new System.Windows.Forms.Padding(4);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(301, 39);
+            this.txtOperand2.TabIndex = 6;
+            this.txtOperand2.Text = "0.00";
+            this.txtOperand2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtOperand2_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(37, 94);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Number 2";
+            // 
+            // btnSubstract
+            // 
+            this.btnSubstract.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubstract.Location = new System.Drawing.Point(180, 236);
+            this.btnSubstract.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSubstract.Name = "btnSubstract";
+            this.btnSubstract.Size = new System.Drawing.Size(132, 53);
+            this.btnSubstract.TabIndex = 7;
+            this.btnSubstract.Text = "-";
+            this.btnSubstract.UseVisualStyleBackColor = true;
+            this.btnSubstract.Click += new System.EventHandler(this.btnSubstract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(333, 236);
+            this.btnMultiply.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(132, 53);
+            this.btnMultiply.TabIndex = 8;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivide.Location = new System.Drawing.Point(487, 236);
+            this.btnDivide.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(132, 53);
+            this.btnDivide.TabIndex = 9;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(555, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 36);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Bersih";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(555, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 36);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Keluar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 343);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubstract);
+            this.Controls.Add(this.txtOperand2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtOperand1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOperand1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOperand2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSubstract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ SimpleCalculator/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        double operand1, operand2, Result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            operand1 = Convert.ToDouble(txtOperand1.Text);
+            operand2 = Convert.ToDouble(txtOperand2.Text);
+            Result = operand1 + operand2;
+            txtResult.Text = Result.ToString();
+        }
+
+        private void btnSubstract_Click(object sender, EventArgs e)
+        {
+            operand1 = Convert.ToDouble(txtOperand1.Text);
+            operand2 = Convert.ToDouble(txtOperand2.Text);
+            Result = operand1 - operand2;
+            txtResult.Text = Result.ToString();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            operand1 = Convert.ToDouble(txtOperand1.Text);
+            operand2 = Convert.ToDouble(txtOperand2.Text);
+            Result = operand1 * operand2;
+            txtResult.Text = Result.ToString("#0.00");//Formate  to 2 decimal number
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            //prevent zero division
+            if (Convert.ToDouble(txtOperand2.Text) != 0)
+            {
+                operand1 = Convert.ToDouble(txtOperand1.Text);
+                operand2 = Convert.ToDouble(txtOperand2.Text);
+                Result = operand1 / operand2;
+                txtResult.Text = Result.ToString();
+            }
+            else
+            {
+
+            }
+        }
+
+        //Prevent user from entering non-digit number
+        private void txtOperand1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == false && char.IsControl(e.KeyChar) == false && (e.KeyChar == '.' ? txtOperand1.Text.Contains('.') == true : true))
+                e.Handled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtOperand2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == false && char.IsControl(e.KeyChar) == false && (e.KeyChar == '.' ? txtOperand2.Text.Contains('.') == true : true))
+                e.Handled = true;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            txtOperand1.Text = String.Empty;
+            txtOperand2.Text = "";
+            txtResult.Text = "";
+        }
+
+    }
+}
